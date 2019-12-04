d8f5589313e2bb093500b2c05cbc07be547ce1e3
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-12-03 22:42:30 -0600

    begining of app scaffolded

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+const MainDiv = styled.div`
+background:#011627;
+height:93vh;
+width:100vw;
+`
+
+export default function Home() {
+    return (
+        <MainDiv>
+            
+        </MainDiv>
+    )
+}
