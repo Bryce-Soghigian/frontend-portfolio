dc213030ce0e01809abd6d30c50b53323de9188f
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-01-20 08:54:19 -0600

    Finished the skills section

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,83 +1,82 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 const Container = styled.div`
-display:flex;
-width: calc(100vw - (100vw - 100%));
-height:7vh;
-background:#011627;
-/* border-bottom: 5px #4B6478; */
-`
+  display: flex;
+  width: calc(100vw - (100vw - 100%));
+  height: 7vh;
+  background: #011627;
+  /* border-bottom: 5px #4B6478; */
+`;
 const Left = styled.div`
-display:flex;
-justify-content:start;
-width:50vw;
+  display: flex;
+  justify-content: start;
+  width: 50vw;
 
-background:#011627;
-border-bottom: 1px solid #4B6478;
-`
+  background: #011627;
+  border-bottom: 1px solid #4b6478;
+`;
 const Right = styled.div`
-width:50vw;
+  width: 50vw;
 
-display:flex;
-flex-direction:row-reverse;
-justify-content:end;
-align-items:center;
-background:#011627;
-border-bottom: 1px solid #4B6478;
-`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: end;
+  align-items: center;
+  background: #011627;
+  border-bottom: 1px solid #4b6478;
+`;
 const Name = styled(Link)`
-/* margin:5px; */
-display:flex;
-justify-content:center;
-align-items:center;
-color:white;
-text-transform:uppercase;
-font-family: 'Changa One';
-text-decoration:none;
-margin:5px;
-@media(min-width:1700px){
+  /* margin:5px; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  text-transform: uppercase;
+  font-family: "Changa One";
+  text-decoration: none;
+  margin: 5px;
+  @media (min-width: 1700px) {
     font-size: 3rem;
-}
-:hover{
-    transition:0.5s;
-    color:#61DAFB;
-
-}
-
-`
+  }
+  :hover {
+    transition: 0.5s;
+    color: #61dafb;
+  }
+`;
 
 const NavItem = styled(Link)`
-display:flex;
-margin:5px;
-@media(min-width:800px){
-    margin-right:20px;
-}
+  display: flex;
+  margin: 5px;
+  @media (min-width: 800px) {
+    margin-right: 20px;
+  }
 
-color:#4B6478;
-font-family: 'Changa One';
-text-decoration:none;
-@media(min-width:1700px){
+  color: #4b6478;
+  font-family: "Changa One";
+  text-decoration: none;
+  @media (min-width: 1700px) {
     font-size: 2rem;
-}
-:hover{
-    transition:0.5s;
-    color:white;
-}
-`
+  }
+  :hover {
+    transition: 0.5s;
+    color: white;
+  }
+`;
+
 
 export default function Navbar() {
-    return (
-            <Container>
-                <Left>
-                    <Name to="/">Bryce Soghigian</Name>
-                </Left>
-                <Right>
-                <NavItem to="/Contact">Contact</NavItem>
-                        <NavItem to="/Projects">Projects</NavItem>
-                        
-                        
-                </Right>
-            </Container>
-    )
+  return (
+    <Container>
+      <Left>
+        <Name to="/">Bryce Soghigian</Name>
+      </Left>
+      <Right>
+
+        <NavItem to="/Contact">Contact</NavItem>
+        <NavItem to="/Skills">Skills</NavItem>
+        <NavItem to="/Projects">Projects</NavItem>
+      </Right>
+    </Container>
+  );
 }
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,9 +1,273 @@
-import React from 'react'
+import React from "react";
+import {FaReact,FaNodeJs,FaPython,FaSass,FaCss3Alt} from 'react-icons/fa'
+import{DiJavascript1,DiPostgresql,DiHtml5} from 'react-icons/di'
+import {GiMoebiusTriangle,GiFizzingFlask} from 'react-icons/gi'
+import styled from "styled-components";
+const Container = styled.div`
+  height: 93vh;
+  width: 100vw;
+  background:#011627;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`;
+
+const Bottom = styled.div`
+
+`
+const SkillContainer = styled.div`
+display:flex;
+flex-direction:column;
+margin:2.5%;
+
+
+
+`
+const SkillsContainer = styled.div`
+display:flex;
+justify-content:space-around;
+
+`
+//============Icons=============//
+const NodeWrapper = styled.a`
+color:#ADDB67;
+font-size:2rem;
+@media(min-width:1500px){
+
+font-size: 3rem;
+}
+@media(min-width:1700px){
+
+font-size: 5rem;
+}
+
+`
+const ThreeHeader = styled.a`
+color:white;
+font-size:2rem;
+
+@media(min-width:1500px){
+
+font-size: 3rem;
+}
+@media(min-width:1700px){
+
+font-size: 5rem;
+}
+`
+const ReactWrapper = styled.a`
+color:rgb(97,218,251);
+font-size:2rem;
+@media(min-width:1500px){
+
+font-size: 3rem;
+}
+@media(min-width:1700px){
+
+font-size: 5rem;
+}
+
+`
+const JavascriptHeader = styled.a`
+color:#f7df1e;
+font-size:2rem;
+@media(min-width:1500px){
+
+font-size: 3rem;
+}
+@media(min-width:1700px){
+
+font-size: 5rem;
+}
+
+
+`
+const PythonWrapper = styled.a`
+color:#4B8BBE;
+/* color:#0D73D9; */
+font-size:2rem;
+@media(min-width:1500px){
+
+font-size: 3rem;
+}
+@media(min-width:1700px){
+
+font-size: 5rem;
+}
+
+`
+const SqlWrapper = styled.a`
+color:#0D73D9;
+font-size:2rem;
+@media(min-width:1500px){
+
+font-size: 3rem;
+}
+@media(min-width:1700px){
+
+font-size: 5rem;
+}
+
+
+`
+const CssWrapper = styled.a`
+color:#BF4080; 
+font-size:2rem;
+@media(min-width:1500px){
+
+font-size: 3rem;
+}
+@media(min-width:1700px){
+
+font-size: 5rem;
+}
+
+
+`
+const HtmlWrapper = styled.a`
+color:#F16529;
+font-size:2rem;
+@media(min-width:1500px){
+
+font-size: 3rem;
+}
+@media(min-width:1700px){
+
+font-size: 5rem;
+}
+
+`
+//==========Headers===========//
+const HtmlHeader = styled.h3`
+color:#F16529;
+margin:0;
+`
+const CssHeader = styled.h3`
+color:#BF4080; 
+margin:0;
+
+
+`
+const LightBlueHeader = styled.h3`
+color:#0D73D9;
+margin:0;
+
+`
+const DarkBlueHeader = styled.h3`
+color:#4B8BBE;
+margin: 0;
+`
+const BlueHeader = styled.h3`
+color:rgb(97,218,251);
+margin:0;
+`
+const GreenHeader = styled.h3`
+color:#ADDB67;
+margin:0;
+
+`
+const YellowHeader = styled.h3`
+color:#f7df1e;
+
+margin:0;
+
+`
+const WhiteHeader = styled.h3`
+color:white;
+margin:0;
+`
+const SkillsText = styled.h1`
+font-size:7rem;
+color:white;
+font-family: 'Bangers';
+
+
+`
+
 
 export default function Skills() {
-    return (
-        <div>
-            Testing
-        </div>
-    )
+return(
+    <Container>
+
+        <Bottom>
+
+
+
+            <SkillsContainer>
+                <SkillContainer>
+                    <ReactWrapper>
+                        <FaReact/>
+                    </ReactWrapper>
+                    <BlueHeader>
+                        ReactJS
+                    </BlueHeader>
+                    
+                </SkillContainer>
+                <SkillContainer>
+                    <NodeWrapper>
+                        <FaNodeJs/>
+                    </NodeWrapper>
+                    <GreenHeader>
+                        NodeJS
+                    </GreenHeader>
+                </SkillContainer>
+                <SkillContainer>
+                    <JavascriptHeader>
+                        <DiJavascript1/>
+                    </JavascriptHeader>
+                    <YellowHeader>
+                        Javascript
+                    </YellowHeader>
+                </SkillContainer>
+                <SkillContainer>
+                    <ThreeHeader>
+                        <GiMoebiusTriangle/>
+                    </ThreeHeader>
+                    <WhiteHeader>
+                        ThreeJS
+                    </WhiteHeader>
+                </SkillContainer>
+            </SkillsContainer>
+            <SkillsText>SKILLS</SkillsText>
+
+            <SkillsContainer>
+                <SkillContainer>
+                    <SqlWrapper>
+                        <DiPostgresql/>
+                    </SqlWrapper>
+                    <LightBlueHeader>
+                        Postgres
+                    </LightBlueHeader>
+                </SkillContainer>
+                <SkillContainer>
+                    <HtmlWrapper>
+                        <DiHtml5/>
+                    </HtmlWrapper>
+                    <HtmlHeader>
+                        HTML/CSS
+                    </HtmlHeader>
+                </SkillContainer>
+                <SkillContainer>
+                    <CssWrapper>
+                        <FaSass/>
+                    </CssWrapper>
+                    <CssHeader>
+                        Sass/Less
+                    </CssHeader>
+                </SkillContainer>
+                <SkillContainer>
+                    <PythonWrapper>
+                        <FaPython/>
+                    </PythonWrapper>
+                    <DarkBlueHeader>
+                        Python
+                    </DarkBlueHeader>
+                </SkillContainer>
+            </SkillsContainer>
+
+
+
+        </Bottom>
+    </Container>
+)
 }
