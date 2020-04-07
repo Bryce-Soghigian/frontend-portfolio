ff931d69ce61c1f687bf2458186d07198585ccaa
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-04-07 13:45:08 -0500

    completed react planetary project card

--- src/Components/Projects/ProjectFive.js
+++ src/Components/Projects/ProjectFive.js
@@ -49,6 +49,40 @@ const Description = styled.p`
 color:white;
 
 `
+const ButtonsContainer = styled.div`
+
+display:flex;
+justify-content:center;
+
+
+`
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.7rem;
+  margin: 1em;
+  width: 7vw;
+
+  height: 3vh;
+  border-radius: 5px;
+  border-style: solid;
+  border-color: black;
+  background: #61dbfb;
+  font-family: "Changa One";
+  cursor: pointer;
+  min-width: 50px;
+  :hover {
+    color: white;
+    border-style: solid;
+    border-color: white;
+    transform: scale(1.5);
+    transition: 0.5s;
+  }
+`;
+const A = styled.a`
+  text-decoration: none;
+`;
 export default function ProjectFive() {
   return (
     <div>
@@ -60,17 +94,41 @@ export default function ProjectFive() {
             </ProjectTitle>
             <Description>
                 React planetary is an npm module that I built that allows you to import entire planets into your application with one line of code. 
-                
+                <br></br>
+                 NPM | THREEJS | REACT | STORYBOOK
             </Description>
+            <ButtonsContainer>
+          <A
+          href="https://heuristic-mestorf-b06f09.netlify.com/"
+          target="_blank"
+          >
+            <Button>
+              Demo
+            </Button>
+            </A>
+            <A
+            href="https://github.com/Bryce-Soghigian/react-planetary"
+            target="_blank"
+            >
+              <Button>
+                Code
+              </Button>
+            </A>
+            <A
+            href="https://www.npmjs.com/package/react-planetary"
+            target="_blank"
+            >
+              <Button>
+                NPM
+              </Button>
+            </A>
+            </ButtonsContainer>
         </ProjectRight>
         <ProjectLeft>
             <ImageContainer src={react_planetary}/>
         </ProjectLeft>
 
 
-
-        </ProjectContainer>
-        <ProjectContainer>
 
         </ProjectContainer>
 
